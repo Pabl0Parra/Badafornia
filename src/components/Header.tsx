d100309d61f54d8a6b56a2eb6c6146e200102bc6
Pabl0Parra <pabsblue16@gmail.com>
2023-06-28 23:37:01 +0200

    finish basic Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,25 @@
-// components/Header/index.tsx
 import { useState } from "react";
+import Link from "next/link";
 import { motion } from "framer-motion";
 import styles from "../../styles/Header.module.css";
 
 export const Header = () => {
   const [activeLink, setActiveLink] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
   const links = ["Home", "Shop", "Contact"];
+
+  const Hamburger = () => {
+    return (
+      <button
+        onClick={() => setMenuOpen(!menuOpen)}
+        className={styles.hamburger}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    );
+  };
 
   return (
     <header className={styles.header}>
@@ -13,27 +27,35 @@ export const Header = () => {
         className={styles.logo}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
+        transition={{ duration: 3 }}
       >
         Logo
       </motion.div>
 
-      <nav className={styles.nav}>
+      <nav className={`${styles.nav} ${menuOpen ? styles.open : ""}`}>
         {links.map((link, index) => (
-          <motion.a
-            key={index}
-            onClick={() => setActiveLink(index)}
-            className={`${styles.link} ${
-              activeLink === index && styles.active
-            }`}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: index * 0.2, duration: 0.5 }}
-          >
-            {link}
-          </motion.a>
+          <Link href={`/${link.toLowerCase()}`} key={index}>
+            <motion.div
+              onClick={() => {
+                setActiveLink(index);
+                setMenuOpen(false);
+              }}
+              className={`${styles.link} ${
+                activeLink === index ? styles.active : ""
+              }`}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: index * 0.2, duration: 0.5 }}
+            >
+              {link}
+            </motion.div>
+          </Link>
         ))}
       </nav>
+
+      <div className={styles.mobileNav}>
+        <Hamburger />
+      </div>
     </header>
   );
 };
